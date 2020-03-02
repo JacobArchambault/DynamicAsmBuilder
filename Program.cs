be2f3718a4ecf34e4eb95e2cc3ab65475791ee81
Jacob Archambault <jacob.archambault@waystar.com>
2020-03-01 23:26:22 -0500

    Clean up code.

--- Program.cs
+++ Program.cs
@@ -1,54 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Threading;
+using static System.Activator;
+using static System.Console;
+using static System.Reflection.Assembly;
+using static System.Threading.Thread;
 
 namespace DynamicAsmBuilder
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("***** The Amazing Dynamic Assembly Builder App *****");
+            WriteLine("***** The Amazing Dynamic Assembly Builder App *****");
             // Get the application domain for the current thread.
-            AppDomain curAppDomain = Thread.GetDomain();
+            AppDomain currentAppDomain = GetDomain();
 
             // Create the dynamic assembly using our helper f(x).
-            CreateMyAsm(curAppDomain);
-            Console.WriteLine("-> Finished creating MyAssembly.dll.");
+            CreateMyAsm(currentAppDomain);
+            WriteLine("-> Finished creating MyAssembly.dll.");
 
             // Now load the new assembly from file.
-            Console.WriteLine("-> Loading MyAssembly.dll from file.");
-            Assembly a = Assembly.Load("MyAssembly");
+            WriteLine("-> Loading MyAssembly.dll from file.");
+            Assembly assembly = Load("MyAssembly");
 
             // Get the HelloWorld type.
-            Type hello = a.GetType("MyAssembly.HelloWorld");
+            Type hello = assembly.GetType("MyAssembly.HelloWorld");
 
             // Create HelloWorld object and call the correct ctor.
-            Console.Write("-> Enter message to pass HelloWorld class: ");
-            string msg = Console.ReadLine();
-            object[] ctorArgs = new object[1];
-            ctorArgs[0] = msg;
-            object obj = Activator.CreateInstance(hello, ctorArgs);
+            Write("-> Enter message to pass HelloWorld class: ");
+            string message = ReadLine();
+            object[] constructorArgs = new object[1];
+            constructorArgs[0] = message;
+            object obj = CreateInstance(hello, constructorArgs);
 
             // Call SayHello and show returned string.
-            Console.WriteLine("-> Calling SayHello() via late binding.");
-            MethodInfo mi = hello.GetMethod("SayHello");
-            mi.Invoke(obj, null);
+            WriteLine("-> Calling SayHello() via late binding.");
+            MethodInfo methodInfo = hello.GetMethod("SayHello");
+            methodInfo.Invoke(obj, null);
 
             // Invoke method.
-            mi = hello.GetMethod("GetMsg");
-            Console.WriteLine(mi.Invoke(obj, null));
+            methodInfo = hello.GetMethod("GetMsg");
+            WriteLine(methodInfo.Invoke(obj, null));
 
         }
 
-        #region Create Assembly
         // The caller sends in an AppDomain type.
-        public static void CreateMyAsm(AppDomain curAppDomain)
+        public static void CreateMyAsm(AppDomain currentAppDomain)
         {
             // Establish general assembly characteristics.
             AssemblyName assemblyName = new AssemblyName
@@ -59,7 +57,7 @@ namespace DynamicAsmBuilder
 
             // Create new assembly within the current AppDomain.
             AssemblyBuilder assembly =
-              curAppDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
+              currentAppDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
 
             // Given that we are building a single-file
             // assembly, the name of the module is the same as the assembly.
@@ -119,8 +117,5 @@ namespace DynamicAsmBuilder
             // (Optionally) save the assembly to file.
             assembly.Save("MyAssembly.dll");
         }
-
-        #endregion
-
     }
 }
